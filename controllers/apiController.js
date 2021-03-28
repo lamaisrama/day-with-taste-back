07204ded32bd9dc7db93892fdda242e086434001
primewinter <yuuni.cha@gmail.com>
2021-03-28 23:30:04 +0900

    feat: youtube 음악 검색 기능 추가

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,13 @@
 import Visitor from "../models/Visitor";
 import Result from "../models/Result";
 import { json } from "body-parser";
+import Youtube from "youtube-node";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const youtube = new Youtube();
+youtube.setKey(process.env.YOUTUBE_KEY);
 
 export const updateVisitorCount = async (req, res) => {
   let today = getYearMonthDate();
@@ -17,6 +24,47 @@ export const updateVisitorCount = async (req, res) => {
     }
   );
 };
+
+export const searchMusic = async (req, res, next) => {
+  // x-www-form-urlencoded 만 가능
+  // TODO : HTTP 지정해서 json으로 전송 받을 것
+  var pageToken = req.body.pageToken;
+  youtube.addParam('order', 'relevance'); // 관련성 순서
+  youtube.addParam('type', 'video'); // 타입 지정 
+  youtube.addParam('part', 'snippet');
+  youtube.addParam('regionCode', 'KR');
+  youtube.addParam('safeSearch', 'moderate');
+  youtube.addParam('pageToken', pageToken);
+  var limit = 5;
+  var word = req.body.keyword;
+
+  console.log('검색어 : '+word);
+  console.log('=======================================')
+  youtube.search(word, limit, function (err, result) { // 검색 실행 
+      if (err) { 
+          console.log(err); 
+          return res.status(500).json(err); 
+      } // 에러일 경우 에러공지하고 빠져나감 
+      var nextPageToken = result.nextPageToken;
+      var prevPageToken = result.prevPageToken;
+      var items = result["items"]; // 결과 중 items 항목만 가져옴 
+      var list = [];
+      for (var i in items) { 
+          var it = items[i]; 
+          var title = it["snippet"]["title"]; 
+          var video_id = it["id"]["videoId"]; 
+          var url = "https://www.youtube.com/watch?v=" + video_id; 
+          var thumbnails = it["snippet"]["thumbnails"];
+          list.push({title: title, video_id: video_id, url: url, thumbnails: thumbnails});
+          console.log("제목 : " + title); 
+          console.log("URL : " + url); 
+          console.log("-----------"); 
+      }
+      var info = {item:list, nextPage:nextPageToken, prevPage:prevPageToken};
+      return res.status(200).json(info);
+  });
+}
+
 
 export const saveResult = async (req, res, next) => {
   const {
--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -3,6 +3,7 @@ import {
   findRandomMusic,
   saveResult,
   updateVisitorCount,
+  searchMusic
 } from "../controllers/apiController";
 
 const apiRouter = express.Router();
@@ -11,6 +12,6 @@ apiRouter.get("/", (req, res) => {
   res.send("hi");
 });
 apiRouter.get("/updateVisitorCount", updateVisitorCount);
-
+apiRouter.post("/youtube", searchMusic);
 apiRouter.post("/result", saveResult, findRandomMusic);
 export default apiRouter;
