9dec346aadf57b62cab38f5cbb6ce2f6b08d0f63
primewinter <yuuni.cha@gmail.com>
2021-04-12 15:52:49 +0900

    feat: Last.FM API 음악검색 기능 추가

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,6 +2,7 @@ import Visitor from "../models/Visitor";
 import Result from "../models/Result";
 import Youtube from "youtube-node";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
@@ -69,6 +70,47 @@ export const searchYoutubeMusic = async (req, res, next) => {
 };
 
 export const searchMusic = async (req, res, next) => {
+  var word = encodeURI(req.query.keyword);
+  var limit = 5;
+  console.log('Last.FM API 검색어 : ',req.query.keyword);
+  var url = `http://ws.audioscrobbler.com/2.0/?method=track.search\
+              &track=${word}\
+              &api_key=${process.env.LASTFM_KEY}\
+              &limit=${limit}\
+              &format=json`
+  await axios({
+    method: 'get',
+    url: url,
+    responseType: 'json'
+  }).then((musicList) => {
+    const track = musicList.data.results.trackmatches.track;
+    var list = [];
+    console.log(track);
+    console.log('===========');
+    for(var i in track) {
+      var title = track[i].name;
+      var artist = track[i].artist;
+      var image = [];
+      console.log('이름:', title);
+      console.log('아티스트:', artist);
+      console.log('이미지:',track[i].image[1]);
+      for(var j in track[i].image) {
+        var img = track[i].image[j];
+        image.push(img);
+        // console.log('image'+j, img);
+      }
+      list.push({
+        title: title,
+        artist: artist,
+        image: image,
+      });
+      console.log('--------')
+    }
+    return res.status(200).json(list);
+  })
+  .catch((err) => {
+    return res.status(500).json(err);
+  });
   
 }
 
